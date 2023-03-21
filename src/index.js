29153846806ea52858277d3eb5630f411dc780b0
Anita <sharma.anita00001@gmail.com>
2023-03-21 20:52:58 +0545

    Module: fetchFromAPI

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
  * Author: Abdullah Al Mamun <mamun1214@gmail.com>,Anita Sharma
  ****************************************** */
 import './css/apps.css';
+import '.css/home.css';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.min';
 import Apps from './modules/Apps';
---
+++ src/modules/fetchFromAPI.js
@@ -0,0 +1,5 @@
+const baseURL = 'https://api.artic.edu/api/v1/';
+const para = 'artworks?fields=id,title,artwork_type_title,thumbnail';
+
+const URL = baseURL + para;
+
